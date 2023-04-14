670839a555d4e373fd8dbba99c0af685aa380b90
Brandon Guo <b.guo55@gmail.com>
2023-04-14 16:27:22 -0400

    EDA

--- src/EDA.py
+++ src/EDA.py
@@ -14,6 +14,7 @@ def main():
     # Retrieve entire dataset and enumerate
     data = Dataset.readCSV("./utils/iris_original.csv")
     label_map = {"Iris-setosa":1, "Iris-versicolor":2, "Iris-virginica":3}
+    col_headers = ["Sepal Length", "Sepal Width", "Petal Length", "Petal Width"]
     reversed_map = dict([(value, key) for key, value in label_map.items()])
     [x, y] = Dataset.splitAndEnumData(data, label_map)
 
@@ -25,7 +26,34 @@ def main():
             y_idx = np.where(y == j)[0]
             x_by_class = np.transpose(x[y_idx,i])
             plt.hist(x_by_class, alpha=0.5, label=str.format("{0}",reversed_map[j]))
+        plt.title(col_headers[i])
+        plt.xlabel("(cm)")
         plt.legend(loc='upper right')
+    plt.show()
+
+    # Plot Sepal Length and Width
+    plt.subplot(1,2,1)
+    for j in range(1,4):
+        y_idx = np.where(y == j)[0]
+        sepal_length = np.transpose(x[y_idx,0])
+        sepal_width = np.transpose(x[y_idx,1])
+        plt.scatter(sepal_length, sepal_width, label=str.format("{0}",reversed_map[j]))
+    plt.title("Sepal Width vs. Sepal Length")
+    plt.xlabel("Sepal Length (cm)")
+    plt.ylabel("Sepal Width (cm)")
+    plt.legend(loc='upper right')
+
+    # Plot Petal Length and Width
+    plt.subplot(1,2,2)
+    for j in range(1,4):
+        y_idx = np.where(y == j)[0]
+        petal_length = np.transpose(x[y_idx,2])
+        petal_width = np.transpose(x[y_idx,3])
+        plt.scatter(petal_length, petal_width, label=str.format("{0}",reversed_map[j]))
+    plt.title("Petal Width vs. Petal Length")
+    plt.xlabel("Petal Length (cm)")
+    plt.ylabel("Petal Width (cm)")
+    plt.legend(loc='upper right')
     plt.show()
 
 if __name__ == "__main__":
